def4407f5d59fcf41cea2bc0137bf20deb5ecb4d
TommyT1225 <takuyamarumoco@gmail.com>
2024-02-03 01:25:03 +0900

    Active Storageの導入

--- README.md
+++ README.md
@@ -22,15 +22,15 @@ has_many :purchasing_infos
 ## items
 | Column          | Type      | Options                     |
 | ----------------| --------- | -------------------------   | 
-|item_name	      |string	    |null: false|
-|description	    |text	      |null: false|
+|name	            |string	    |null: false|
+|content    	    |text	      |null: false|
 |price	          |integer	  |null: false|
 |user	            |references	|null: false, foreign_key:true|
 |category_id	    |integer	  |null: false|
-|condition_id	    |integer	  |null: false|
-|shipping_fee_id	|integer	  |null: false|
-|ship_from_id	    |integer	  |null: false|
-|shipping_date_id	|integer	  |null: false|
+|situation_id	    |integer	  |null: false|
+|fee_id	          |integer	  |null: false|
+|area_id	        |integer	  |null: false|
+|delivery_id	    |integer	  |null: false|
 
 Association
 belongs_to :user
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,61 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :same_user, only: [:edit, :destroy]
+
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    if Order.exists?(item_id: @item.id)
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:content,:category_id,:situation_id,:fee_id,:area_id,:delivery_id,:price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def same_user
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
 end
