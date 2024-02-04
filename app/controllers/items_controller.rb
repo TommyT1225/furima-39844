0f520a037f78cc4c69521a954cef7d8d2f3bd85b
TommyT1225 <takuyamarumoco@gmail.com>
2024-02-04 20:40:32 +0900

    途中経過

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :same_user, only: [:edit, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
